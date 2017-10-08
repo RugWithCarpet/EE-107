9796798828b87059db2bf49373ec8f3ff3f7efb1
Vickram Gidwani <vickgidwani@gmail.com>
2017-10-08 13:49:48 -0700

    Added Project Files

---
+++ Blinkenlights/led.c
@@ -0,0 +1,16 @@
+/*
+ * led.c
+ *
+ *  Created on: Oct 6, 2017
+ *      Author: Vickram Gidwani
+ */
+
+#include "led.h"
+
+void led_init();
+
+void led_on();
+
+void led_off();
+
+void led_toggle();
