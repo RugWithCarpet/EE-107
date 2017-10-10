d6ab56043c44e150e3481928a5bc85f0694fa880
RugWithCarpet <akucy@stanford.edu>
2017-10-09 22:48:34 -0700

    Cleaned up format

--- Blinkenlights/led.c
+++ Blinkenlights/led.c
@@ -13,36 +13,19 @@ void led_init(){
         P3DIR |= 0b00100000; //set port 3 pin 5 to output mode
         }
 
-void led_on (){
-    P3OUT  = 0b00000000;
+void led_on (){     //turn LED on
+    P3OUT  = 0b00000000; //set port 3 pin 5 active-low
 };
 
-void led_off (){
-    P3OUT  = 0b00100000;
+void led_off (){    //turn LED off
+    P3OUT  = 0b00100000; //set port 3 pin 5 deactive-high
 };
 
-void led_toggle (){
-
+void led_toggle (){     //toggle LED pin
     if (P3OUT == 0b00000000) {
-        printf("P3OUT = 0b00000000");
+        P3OUT = 0b00100000;  //set port 3 pin 5 deactive-high
     }
     else {
-        printf("P3OUT = 0b00100000");
+        P3OUT = 0b00000000;  //set port 3 pin 5 active-low
     }
-
-    if (P3OUT == 0b00000000) {
-        P3OUT = 0b00100000;
-    }
-    else {
-        P3OUT = 0b00000000;
-    }
-    P3OUT ? 0b00100000 : 0b00000000;
-
-    if (P3OUT == 0b00000000) {
-           printf("P3OUT = 0b00000000");
-       }
-    else {
-           printf("P3OUT = 0b00100000");
-    }
-
 };
--- Blinkenlights/main.c
+++ Blinkenlights/main.c
@@ -11,21 +11,8 @@ int main(void)
 {
     led_init();
 
-//    int j = 0;
-//    for (j = 0; j < 10; j++) {
-//        led_on();
-//
-//        int i = 0;
-//        for (i = 0; i < 10000; i++) { }
-//
-//        led_toggle();
-//    }
-
     led_on();
     led_toggle();
-
-
-
 
 	return 0;
 
