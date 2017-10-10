e97150ff866102fa1172d89ec2af470603709214
Vickram Gidwani <vickgidwani@gmail.com>
2017-10-09 22:41:06 -0700

    timer basic setup

--- Blinkenlights/timer.c
+++ Blinkenlights/timer.c
@@ -6,3 +6,17 @@
  */
 
 #include "timer.h"
+
+void timerA0_init() {
+    //TA0CTL =  //set clock origin with TASSEL (want crystal)
+    //TA0R //counter register
+    //TA0CCCTL0 //capture/compare 0 control register, up mode,
+};
+
+void timerA0_reset() {
+    //TA0R or TA0CTL
+};
+
+void timerA0_set(uint16_t period) {
+    TA0CCR0 = period;
+};
