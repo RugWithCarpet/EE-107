c4aa55d29319a1f50c2df70eee3eb465cf6f356f
Vickram Gidwani <vickgidwani@gmail.com>
2017-11-30 16:52:29 -0800

    Started getting ADC file to work

--- adc.c
+++ adc.c
@@ -8,4 +8,53 @@
 
 #include "adc.h"
 
-void adc_init();
+void adc_init() {
+
+	//Setup microphone on pin 1.5 (A5) as analog input
+	GPIO_setAsPeripheralModuleFunctionInputPin(
+			GPIO_PORT_P1,
+			GPIO_PIN5,
+			GPIO_TERNARY_MODULE_FUNCTION
+	);
+
+
+	//14 clock cycles for 12 bit conversion
+	//2 clock cycles for between sampling and conversion
+	//32 clock cycles for sampling/hold
+	//minimum of 50 clock cycles per period
+	//4 MHz SMCLK
+	//32 kHz sampling clock
+
+	Timer_A_initUpModeParam timer_param = {0};
+	timer_param.clockSource = TIMER_A_CLOCKSOURCE_SMCLK;
+	timer_param.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
+	timer_param.startTimer = FALSE;
+	timer_param.timerPeriod = 125;
+	timer_param.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_ENABLE;
+	timer_param.captureCompareInterruptEnable_CCR0_CCIE = TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE;
+
+
+	//set up TimerA0 Compare/Control Register 1 as 32 kHz clock
+	Timer_A_initUpMode(
+			TA0_BASE,
+
+	);
+
+
+	ADC12_B_initParam adc_param = {0};
+	adc_param.clockSourceDivider = ADC12_B_CLOCKDIVIDER_1;
+	adc_param.clockSourceSelect = ADC12_B_CLOCKSOURCE_SMCLK;
+	adc_param.clockSourcePredivider = ADC12_B_CLOCKPREDIVIDER__1;
+	adc_param.internalChannelMap = ADC12_B_NOINTCH;
+	adc_param.sampleHoldSignalSourceSelect = ADC12_B_SAMPLEHOLDSOURCE_1; //TimerA0 Compare/Control Register 1
+	ADC12_B_init(ADC12_B_BASE, &adc_param);
+
+	ADC12_B_setupSamplingTimer(
+			ADC12_B_BASE,
+			ADC12_B_CYCLEHOLD_32_CYCLES, //8 us of 4 MHz clock
+			ADC12_B_CYCLEHOLD_32_CYCLES,
+			ADC12_B_MULTIPLESAMPLEDISABLE
+	)
+
+
+}
--- main.c
+++ main.c
@@ -10,6 +10,13 @@ int main(void)
 {
 	WDT_A_hold(WDT_A_BASE); // stop watchdog
 	PMM_unlockLPM5(); //does this have to be done before or after changing pin settings?
+
+	CS_setExternalClockSource(32000, 8000000); //LFXT is 32KHz, HFXT is 8MHz
+	CS_turnOnHFXT(CS_HFXT_DRIVE_4MHZ_8MHZ); //turn on HFXT
+
+    CS_initClockSignal(CS_MCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_1); //Set MCLK to DCO, with divider=1
+    CS_initClockSignal(CS_SMCLK, CS_HFXTCLK_SELECT, CS_CLOCK_DIVIDER_2); //Set SMCLK to DCO, with divider=2
+
 	button_init();
     led_init();
     //adc_init();
