48a11bb4cee38b197cb6ec56cb7e63ee3686accd
rjreyn <32584523+rjreyn@users.noreply.github.com>
2017-12-14 13:46:58 -0800

    updated UART receiver python
    changed file extension to .raw
    NOTE: need to specify what end of file indicator to use

--- UART_RX.py
+++ UART_RX.py
@@ -3,13 +3,13 @@ import time
 ser = serial.Serial("/dev/cu.usbserial-AI05GG94")
 fileNum = 1
 while (1):
-	fileName = "audioTest" + str(fileNum) + ".txt"
+	fileName = "audioTest" + str(fileNum) + ".raw"
 	file = open(fileName,"w")
 	while (1):
 		with serial.Serial("/dev/cu.usbserial-AI05GG94", 9600) as ser:
 			x = ser.read()
-		if (str(x, 'utf-8') == '\n'):
+		if (x == END_OF_FILE_INDICATOR):
 			break
-		file.write(str(x,'utf-8'))
+		file.write(x)
 	file.close()
 	fileNum = fileNum + 1
